c71e2be1a79d7652da760cb6f063965cb8cd1960
João Pedro <joao.casella@fanatee.com>
2022-10-09 16:23:50 -0300

    Implemented the player hability to walk on slopes

--- FPS/Assets/Scripts/PlayerTest.cs
+++ FPS/Assets/Scripts/PlayerTest.cs
@@ -9,6 +9,9 @@ namespace Fps.Controller
         [field: SerializeField]
         private Camera Camera { get; set; }
 
+        [field: SerializeField]
+        private LayerMask FloorLayerMask { get; set; }
+
         [field: SerializeField, Range(0.01f, 2f)]
         private float HorizontalSensitivity { get; set; } = 0.5f;
         [field: SerializeField, Range(0.01f, 2f)]
@@ -28,16 +31,34 @@ namespace Fps.Controller
         [field: SerializeField, Range(0.1f, 2f)]
         private float JumpGravityMultiplier { get; set; } = 1f;
 
+        [field: SerializeField, Range(0.0005f, 0.008f), Tooltip("Also works as the mass")]
+        private float SlopeDecelerationMultiplier { get; set; } = 0.008f;
+
         [field: SerializeField, Range(60f, 120f)]
         private float CameraPitchLimit { get; set; } = 90f;
 
         private CameraRotation CameraRotationEulerAngles { get; set; }
-
+        private Vector3 PlaneNormal { get; set; } = Vector3.up;
+        private float Gravity { get; set; }
+        private float VerticalVelocity { get; set; }
         private Coroutine Jumping { get; set; }
 
         private void Start()
         {
             CameraRotationEulerAngles = new CameraRotation(Camera.transform.localEulerAngles);
+            SetupGravity();
+        }
+
+        private void SetupGravity()
+        {
+            // th is the time will take to take the player to the top of the parabola
+            // This is the reciprocal of that value
+            var oneOverTime = 1f / JumpTime;
+
+            // v0 = 2 * JumpHeight / th
+            VerticalVelocity = 2f * JumpHeight * oneOverTime;
+            // g = -2 * JumpHeight / (th ^ 2)
+            Gravity = -(VerticalVelocity) * oneOverTime;
         }
 
         private void FixedUpdate()
@@ -56,45 +77,55 @@ namespace Fps.Controller
 
             transform.position += Move(forwardMove, rightMove);
 
-            if (Input.GetKey(KeyCode.Space) && Jumping == null)
-                Jumping = StartCoroutine(ParabolicMovement(JumpTime, JumpGravityMultiplier, transform.position.y, null, () => Jumping = null));
+            // TODO: allow the player to jump while on a slope (the last check prevents this behaviour)
+            if (Input.GetKey(KeyCode.Space) && Jumping == null && PlaneNormal == Vector3.up)
+                Jumping = StartCoroutine(ParabolicMovement(JumpGravityMultiplier, transform.position.y, null, () => Jumping = null));
+
+            if (Physics.Raycast(transform.position, -transform.up, out RaycastHit raycastHit, float.MaxValue, FloorLayerMask))
+                PlaneNormal = raycastHit.normal;
+            else
+                PlaneNormal = Vector3.up;
         }
 
-        // TODO: treat values when the player aims completely up/down
         private Vector3 Move(float forwardMovementDirection, float rightMovementDirection)
         {
-            // TODO: get the actual plane normal (for slopes, for example)
-            var planeNormal = Vector3.up;
+            var slopeAtenuation = Vector3.zero;
+            var slopeDeclination = Vector3.Dot(PlaneNormal, Vector3.up);
+
+            if (!Mathf.Approximately(slopeDeclination, 1f))
+            {
+                var planeRight = Vector3.Cross(PlaneNormal, Vector3.up);
+                var planeForward = Vector3.Cross(planeRight, PlaneNormal);
+
+                slopeAtenuation = (float) Math.Acos(slopeDeclination) * SlopeDecelerationMultiplier * Gravity * planeForward;
+            }
 
             var cameraForward = Camera.transform.forward;
             // > 0 if points up (same direction), 0 if points forward, < 0 if points down (oposite directions)
-            var forwardProjectionOnNormal = Vector3.Dot(cameraForward, planeNormal);
-            var forwardRelativeToCamera = cameraForward - (forwardProjectionOnNormal * planeNormal);
+            var forwardProjectionOnNormal = Vector3.Dot(cameraForward, PlaneNormal);
+            var forwardRelativeToCamera = cameraForward - (forwardProjectionOnNormal * PlaneNormal);
 
             var cameraRight = Camera.transform.right;
             // > 0 if points up (same direction), 0 if points forward, < 0 if points down (oposite directions)
-            var rightProjectionOnNormal = Vector3.Dot(cameraRight, planeNormal);
-            var rightRelativeToCamera = cameraRight - (rightProjectionOnNormal * planeNormal);
+            var rightProjectionOnNormal = Vector3.Dot(cameraRight, PlaneNormal);
+            var rightRelativeToCamera = cameraRight - (rightProjectionOnNormal * PlaneNormal);
 
-            return (forwardMovementDirection * ForwardMovementOnPress * forwardRelativeToCamera) + (rightMovementDirection * RightMovementOnPress * rightRelativeToCamera);
+            return (forwardMovementDirection * ForwardMovementOnPress * forwardRelativeToCamera.normalized)
+                + (rightMovementDirection * RightMovementOnPress * rightRelativeToCamera.normalized)
+                + slopeAtenuation;
         }
 
         // (10/09/2022) Based on "Math for Game Programmers: Building a Better Jump", available at: https://www.youtube.com/watch?v=hG9SzQxaCm8
         private IEnumerator ParabolicMovement(
-            float time,
             float descendGravityMultiplier,
             float finalVerticalPosition,
             Action onMovementPeak,
             Action onComplete)
         {
-            // th is the time will take to take the player to the top of the parabola
-            // This is the reciprocal of that value
-            var oneOverTime = 1f / time;
+            SetupGravity();
 
-            // v0 = 2 * JumpHeight / th
-            var verticalVelocity = 2f * JumpHeight * oneOverTime;
-            // g = -2 * JumpHeight / (th ^ 2)
-            var gravity = -(verticalVelocity) * oneOverTime;
+            var verticalVelocity = VerticalVelocity;
+            var gravity = Gravity;
 
             var waitForFixedUpdate = new WaitForFixedUpdate();
             var updatedFallGravity = false;
